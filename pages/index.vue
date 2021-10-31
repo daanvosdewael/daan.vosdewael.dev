782ad0b16c34e0846cf76c5fea47e62d9a4480ad
Daan Vos de Wael <daan@vosdewael.com>
2021-10-31 20:31:15 +0100

    feat: add `index` layout with separate components

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,37 @@
 <template>
-    <Tutorial />
+    <div>
+        <header :class="$style['header']">
+            <Logo />
+            <Intro />
+        </header>
+
+        <footer :class="$style['footer']">
+            <Keywords />
+        </footer>
+    </div>
 </template>
 
-<script>
-export default {};
-</script>
+<style lang="postcss" module>
+.header {
+    align-content: start;
+    display: grid;
+    gap: 56px;
+    margin: 49px 32px 32px;
+
+    @media (--large) {
+        grid-area: 1;
+    }
+}
+
+.footer {
+    margin: 0 32px 32px;
+
+    @media (--large) {
+        align-self: end;
+        grid-area: 1 / 2;
+        justify-self: end;
+        margin: 0 32px 37px;
+        max-width: 600px;
+    }
+}
+</style>
