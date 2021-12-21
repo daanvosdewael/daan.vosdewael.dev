21b334768b48d0362a26cbb68f5b7f8d17d5193d
Daan Vos de Wael <daan@vosdewael.com>
2021-12-21 19:10:00 +0100

    chore: change build tool configs to `json` files

--- lint-staged.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable unicorn/prefer-module */
-
-module.exports = {
-    '*.{js,vue}': 'eslint',
-    '*.{css,vue}': 'stylelint',
-    'package.json': 'npx sort-package-json',
-};
--- simple-git-hooks.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable unicorn/prefer-module */
-
-module.exports = {
-    'pre-commit': 'npx lint-staged',
-    'commit-msg': 'npx --no-install commitlint --edit "$1"',
-};
