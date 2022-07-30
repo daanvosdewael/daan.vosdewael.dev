72119e4c347dc406364092f0740aadc733f74f02
Daan Vos de Wael <daan@vosdewael.com>
2022-07-30 15:25:26 +0200

    fix: change indentation to tabs

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,14 @@ import { createHtmlPlugin } from 'vite-plugin-html';
 import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
-    build: {
-        emptyOutDir: true,
-        outDir: '../dist',
-    },
-    plugins: [
-        viteSingleFile(),
-        createHtmlPlugin(),
-    ],
-    publicDir: '../static',
-    root: './src',
+	build: {
+		emptyOutDir: true,
+		outDir: '../dist',
+	},
+	plugins: [
+		viteSingleFile(),
+		createHtmlPlugin(),
+	],
+	publicDir: '../static',
+	root: './src',
 });
