bf0023185145d4f9dddf0e9d8392b0eff6ccf1c7
Daan Vos de Wael <daan@vosdewael.com>
2025-08-17 15:37:17 +0200

    chore: remove `simple-git-hooks` and `lint-staged` dependencies + configs

--- .lintstagedrc.mjs
+++
@@ -1,5 +0,0 @@
-export default {
-	'**/*.?(m)js': filenames => `eslint ${filenames.join(' ')} --fix`,
-	'**/*.css': filenames => `stylelint ${filenames.join(' ')} --fix`,
-	'package.json': 'npx sort-package-json',
-};
